905dd3c9595f5f93889fc08c37ec97a85d22a9e9
Guest User <Guest@epicodus-10.local>
2017-11-15 13:51:55 -0800

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
 
+  get '/platforms' => 'platforms#index'
+  get '/games' => 'games#index'
+  get '/mechs' => 'gears#index'
+  get '/characters' => 'characters#index'
   get '/about' => 'pages#about'
 
-  resources :platforms
+  resources :platforms, :games, :gears, :characters
+
 end
