6c52b28d566deefeeab2dd1e5b97738e68ce3664
Guest User <Guest@epicodus-39.local>
2017-11-15 11:16:29 -0800

    add mechs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115183553) do
+ActiveRecord::Schema.define(version: 20171115191322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20171115183553) do
   create_table "games_platforms", force: :cascade do |t|
     t.integer "game_id"
     t.integer "platform_id"
+  end
+
+  create_table "metal_gear_mechs", force: :cascade do |t|
+    t.string "name"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "platforms", force: :cascade do |t|
